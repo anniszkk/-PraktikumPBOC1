496739da196d5c3aed5687c5df76d371067f112d
Annis Fakhiroh Akbar <akbarannis3@gmail.com>
2026-03-13 22:36:03 +0700

    Tugas pertemuan 4 fix

--- Pertemuan4/src/MBangunDatar.java
+++ Pertemuan4/src/MBangunDatar.java
@@ -1,32 +1,87 @@
+/*Nama File     : MBangunDatar.java
+  Deskripsi     : berisi atribut dan method dalam class BangunDatar
+  Pembuat       : Annis Fakhiroh Akbar (24060124130110)
+  Lab           : C1
+  Tanggal       : 13 Maret 2026 */
+
 public class MBangunDatar {
     public static void main(String[] args) {
-        BangunDatar BD1 = new BangunDatar();
-        BD1.printInfo();
         System.out.println();
+        System.out.println("------------TEST UNTUK CLASS BANGUNDATAR------------");
+        System.out.println("1. Cek konstruktor tanpa parameter dan berparameter");
+        BangunDatar BD1 = new BangunDatar();
+        BangunDatar BD2 = new BangunDatar(5, "hitam", "hijau");
+        BD1.printInfo();
+        BD2.printInfo();
+        System.out.println();
+        System.out.println("2. Cek mutator untuk bangun datar BD1 yang belum punya data");
         BD1.setJmlSisi(5);
         BD1.setWarna("pink");
         BD1.setBorder("hitam");
         BD1.printInfo();
+        System.out.println();
+        System.out.println("3. Cek selektor untuk bangun datar BD2");
+        System.out.println("Bangun datar BD2 punya jumlah sisi: " + BD2.getJmlSisi());
+        System.out.println("Bangun datar BD2 punya warna: " + BD2.getWarna());
+        System.out.println("Bangun datar BD2 punya border: " + BD2.getBorder());
+        System.out.println();
+        System.out.println("4. Cek method lainnya, yaitu printCounterBangunDatar");
         BangunDatar.printCounterBangunDatar();
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println("------------TEST UNTUK CLASS PERSEGI------------");
+        System.out.println("1. Cek konstruktor tanpa parameter dan berparameter");
         Persegi P1 = new Persegi();
-        Persegi P2 = new Persegi(5, "pink", "hitam");
+        BangunDatar P2 = new Persegi(5, "pink", "hitam");
         P1.printInfo();
         P2.printInfo();
+        System.out.println();
+        System.out.println("2. Cek mutator untuk persegi P1 yang belum punya data");
+        P1.setWarna("kuning");
+        P1.setBorder("biru");
         P1.setSisi(2);
         P1.printInfo();
-        System.out.println("Panjang sisi: " + P2.getSisi());
-        System.out.println("Luas persegi: " + P2.getLuas());
-        System.out.println("Keliling persegi: " + P2.getKeliling());
-        System.out.println("Panjang diagonal persegi: " + P2.getDiagonal());
         System.out.println();
+        System.out.println("3. Cek selektor untuk persegi P2");
+        System.out.println("Persegi P2 punya jumlah sisi: " + P2.getJmlSisi());
+        System.out.println("Persegi P2 punya warna: " + P2.getWarna());
+        System.out.println("Persegi P2 punya border: " + P2.getBorder());
+        System.out.println("Persegi P2 punya panjang sisi: " + ((Persegi)P2).getSisi());
+        System.out.println();
+        System.out.println("4. Cek method lainnya, yaitu:");
+        System.out.println("Luas persegi: " + ((Persegi)P2).getLuas());
+        System.out.println("Keliling persegi: " + ((Persegi)P2).getKeliling());
+        System.out.println("Panjang diagonal persegi: " + ((Persegi)P2).getDiagonal());
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println("------------TEST UNTUK CLASS LINGKARAN------------");
+        System.out.println("1. Cek konstruktor tanpa parameter dan berparameter");
         Lingkaran L1 = new Lingkaran();
-        Lingkaran L2 = new Lingkaran(7, "pink", "hitam");
+        BangunDatar L2 = new Lingkaran(7, "pink", "hitam");
         L1.printInfo();
         L2.printInfo();
+        System.out.println();
+        System.out.println("2. Cek mutator untuk lingkaran L1 yang belum punya data");
+        L1.setWarna("kuning");
+        L1.setBorder("biru");
         L1.setJari(7);
         L1.printInfo();
-        System.out.println("Luas lingkaran: " + L2.getLuas());
-        System.out.println("Keliling lingkaran : " + L2.getKeliling());
-
+        System.out.println();
+        System.out.println("3. Cek selektor untuk lingkaran L2");
+        System.out.println("Lingkaran L2 punya jumlah sisi: " + L2.getJmlSisi());
+        System.out.println("Lingkaran L2 punya warna: " + L2.getWarna());
+        System.out.println("Lingkaran L2 punya border: " + L2.getBorder());
+        System.out.println("Lingkaran L2 punya jari-jari: " + ((Lingkaran)L2).getJari());
+        System.out.println();
+        System.out.println("4. Cek method lainnya, yaitu:");
+        System.out.println("Luas lingkaran: " + ((Lingkaran)L2).getLuas());
+        System.out.println("Keliling lingkaran : " + ((Lingkaran)L2).getKeliling());
     }
 }
--- Pertemuan4/src/Persegi.java
+++ Pertemuan4/src/Persegi.java
@@ -1,10 +1,21 @@
+/*Nama File     : Persegi.java
+  Deskripsi     : berisi atribut dan method dalam class Persegi
+  Pembuat       : Annis Fakhiroh Akbar (24060124130110)
+  Lab           : C1
+  Tanggal       : 13 Maret 2026 */
+
 public class Persegi extends BangunDatar {
+    /************ATRIBUT****************/
     private double sisi;
 
+    /**********METHOD*******************/
+    //KONSTRUKTOR
+    //Konstruktor untuk membuat persegi tanpa parameter
     public Persegi() {
         setJmlSisi(4);
     }
 
+    //konstruktor untuk membuat persegi dengan nilai sisi, warna, dan border tertentu
     public Persegi (double sisi, String warna, String border) {
         super(4,warna,border);
         this.sisi = sisi;
@@ -16,26 +27,35 @@ public class Persegi extends BangunDatar {
         // this.jmlSisi = 4;
     }
 
+    //SELEKTOR
+    //mengembalikan panjang sisi persegi
     public double getSisi() {
         return sisi;
     }
 
+    //MUTATOR
+    //mengeset sisi dengan nilai baru panjang sisi
     public void setSisi (double sisi) {
         this.sisi = sisi;
     }
 
+    //METHOD LAINNYA
+    //mengembalikan luas persegi
     public double getLuas() {
         return sisi*sisi;
     }
 
+    //mengembalikan keliling persegi
     public double getKeliling() {
         return 4*sisi;
     }
 
+    //mengembalikan panjang diagonal persegi
     public double getDiagonal() {
         return sisi*Math.sqrt(2);
     }
 
+    //menampilkan jumlah sisi, warna, border, dan panjang sisi yang dimiliki persegi
     @Override
     public void printInfo() {
         super.printInfo();
