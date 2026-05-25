f14a96c5c881a9356322b4de4fbdaf9635463b95
Annis Fakhiroh Akbar <akbarannis3@gmail.com>
2026-05-25 19:48:48 +0700

    Submit pertemuan 10 fix

---
+++ Pertemuan10/src/PersistentObject/PersonDAO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package PersistentObject;
+
+/**
+ *
+ * @author MyBook Hype
+ */
+/* File : PersonDAO.java
+   Deskripsi : interface untuk person access object
+*/
+public interface PersonDAO {
+    public void savePerson(Person p) throws Exception;
+}
